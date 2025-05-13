202038bd526ca25f98fb689f8584cfafca59cfd9
Enisuzunn <enisbesiktasli@gmail.com>
2025-05-13 15:20:29 +0300

    Initial Commit

---
+++ kitapTakas/kitapTakasApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitapTakasApp.swift
+//  kitapTakas
+//
+//  Created by Enis Uzun on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct kitapTakasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kitapTakasUITests/kitapTakasUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  kitapTakasUITestsLaunchTests.swift
+//  kitapTakasUITests
+//
+//  Created by Enis Uzun on 13.05.2025.
+//
+
+import XCTest
+
+final class kitapTakasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
